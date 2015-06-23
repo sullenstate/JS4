da7553382e64e42acc51df4b6de4e540d9dd7187
Tim <tklein@outlook.com>
2015-06-23 14:29:59 -0600

    Final Commit

--- main.js
+++ main.js
@@ -1,22 +1,56 @@
-var number = prompt('How many victims do you need to enter?');
-
 var victims = [];
 var volunteers = [];
 
-for (i = 1; i <= number; i++) {
+
+// >>>>>>>> Block to query the number of Victims & Volunteers <<<<<<<<
+
+// var number = prompt('How many victims do you need to enter?');
+
+// for (i = 1; i <= number; i++) {
+// 	var name = prompt('Enter the name of victim #' + i);
+// 	var phone = prompt("Enter the victim's phone number:");
+// 	var street = prompt("Enter the victim's street address:");
+// 	var victim = {
+// 		name: name,
+// 		phone: phone,
+// 		street: street,
+// 	}
+// 	victims.push(victim);
+// }
+
+// number = prompt('How many volunteers do you want to enter?');
+// for (i = 1; i <=number; i++) {
+// 	var name = prompt('Enter the name of volunteer #' + i);
+// 	var phone = prompt("Enter the volunteer's phone number:");
+// 	var street = prompt("Enter the volunteer's street address:");
+// 	var volunteer = {
+// 		name: name,
+// 		phone: phone,
+// 		street: street,
+// 	}
+// 	volunteers.push(volunteer);
+
+// }
+// >>>>>>>> End Block <<<<<<<<
+
+// >>>>>>>> Begin Add Confirmation Block <<<<<<<<
+do {
+	var i = 1;
 	var name = prompt('Enter the name of victim #' + i);
 	var phone = prompt("Enter the victim's phone number:");
 	var street = prompt("Enter the victim's street address:");
 	var victim = {
-		name: name,
-		phone: phone,
-		street: street,
+	name: name,
+	phone: phone,
+	street: street,
 	}
 	victims.push(victim);
-}
+	i++;
+	var r = confirm('Press OK to enter more victims or CANCEL to begin adding volunteers');
+} while (r === true);
 
-number = prompt('How many volunteers do you want to enter?');
-for (i = 1; i <=number; i++) {
+do {
+	var i = 1;
 	var name = prompt('Enter the name of volunteer #' + i);
 	var phone = prompt("Enter the volunteer's phone number:");
 	var street = prompt("Enter the volunteer's street address:");
@@ -26,9 +60,12 @@ for (i = 1; i <=number; i++) {
 		street: street,
 	}
 	volunteers.push(volunteer);
+	var r = confirm('Press OK to enter another volunteer or CANCEL if there are no more volunteers');
+	i++;
+} while (r === true);
+// >>>>>>>> End Block <<<<<<<<
 
-}
-
+// >>>>>>>> Begin Alert Block <<<<<<<<
 console.log(victims);
 console.log(volunteers);
 
@@ -51,7 +88,8 @@ alert(
     + 'The following people really need your help...\n\n'
     + '\t• ' + victimString
     + '\n\n'
-    + 'The following people are available to provide assitance...\n\n'
+    + 'The following people are available to provide assistance...\n\n'
     + '\t• ' + volunteerString
     + '\n\n' 
 );
+// >>>>>>>> End Block <<<<<<<<
